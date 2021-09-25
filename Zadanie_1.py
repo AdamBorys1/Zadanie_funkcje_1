9c15be2f39b118138c515d088b78a0606b13f900
Adam Borys <adamborys2002@wp.pl>
2021-09-25 12:44:11 +0200

    Gotowe zadanie

--- Zadanie_1.py
+++ Zadanie_1.py
@@ -1,1 +1,12 @@
-print("hi")
+def palindrom(word):
+    """
+    Ta funkcja sprawdza, czy podany wyraz jest palindromem
+    """
+    word_2 = word[::-1]
+    if word == word_2:
+        return True
+    else:
+        return False
+
+
+print(palindrom("kajak"))
